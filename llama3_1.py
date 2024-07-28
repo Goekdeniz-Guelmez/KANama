ac74b6d739d7c161c43502a4a18f510c57a590c2
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-28 22:33:42 +0200

    other env

--- llama3_1.py
+++ llama3_1.py
@@ -7,6 +7,7 @@ import torch
 
 @dataclass
 class ModelArgs:
+    vocab_size: int = -1
     dim: int = 12
     num_layers: int = 1
     num_heads: int = 6
@@ -16,3 +17,26 @@ class ModelArgs:
 
 
 class Llama3_1(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.args = args
+
+        self.embeddings = nn.Embedding()
+
+        self.layers = nn.ModuleList
+
+        self.norm = nn.BatchNorm1d(args.dim)
+        self.lm_head = nn.Linear(in_features=args.dim, out_features=args.vocab_size, bias=False)
+
+    def forward(self, x: torch.Tensor, start_pos: int) -> torch.Tensor:
+        B, L = x.shape
+
+        embedds = self.embeddings(x)
+
+        freqs_cis = torch.Tensor
+
+        mask = None
+        if L >= 1:
+            print("Implementieren")
+
+        for layer in enumerate()
