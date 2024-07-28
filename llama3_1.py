6b8fa2fbf379d144050a1e23e104903e6a1693fd
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-28 22:44:46 +0200

    update

--- llama3_1.py
+++ llama3_1.py
@@ -10,10 +10,12 @@ import torch.nn as nn
 @dataclass
 class ModelArgs:
     vocab_size: int = -1
+    padding_idx: int = -1
     dim: int = 12
     num_layers: int = 1
     num_heads: int = 6
     num_kv_heads: Optional[int] = None
+    rms_norm_eps: float = 1e-5
 
 
 
@@ -36,22 +38,29 @@ class Llama3_1(nn.Module):
         super().__init__()
         self.args = args
 
-        self.embeddings = nn.Embedding()
+        self.freqs_cis = precompute_freqs_cis() # TODO
 
-        self.layers = nn.ModuleList
+        self.embeddings = nn.Embedding(args.vocab_size, args.dim, padding_idx=)
 
-        self.norm = nn.BatchNorm1d(args.dim)
+        self.layers = torch.nn.ModuleList()
+        for layer_id in range(args.n_layers):
+            self.layers.append(Block(layer_id, args)) # TODO
+
+        self.norm = RMSNorm(args.dim, args.rms_norm_eps)
         self.lm_head = nn.Linear(in_features=args.dim, out_features=args.vocab_size, bias=False)
 
+    @torch.inference_mode()
     def forward(self, tokens: torch.Tensor, start_pos: int) -> torch.Tensor:
         B, L = tokens.shape
         embedds = self.embeddings(tokens)
 
-        freqs_cis = torch.Tensor
+        freqs_cis = # TODO
 
         mask = None
-        if L >= 1:
-            print("Implementieren")
+        if L > 1:
+            mask = torch.full((L, L), float("-inf"), device=tokens.device)
+            mask = torch.triu(mask, diagonal=1)
+            mask = torch.hstack([torch.zeros((L, start_pos), device=tokens.device), mask]).type_as(h)
 
         for layer in self.layers:
             h = layer(h, start_pos, freqs_cis, mask)
