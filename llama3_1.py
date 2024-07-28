e6d4b9b94256dfe895f3ffdbcfbdde7c94ddfe1b
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-28 22:37:38 +0200

    adding RMSNorm

--- llama3_1.py
+++ llama3_1.py
@@ -1,8 +1,10 @@
 from typing import Optional, Tuple
 from dataclasses import dataclass
 
-import torch.nn as nn
+import math
+
 import torch
+import torch.nn as nn
 
 
 @dataclass
@@ -15,6 +17,19 @@ class ModelArgs:
 
 
 
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+    
 
 class Llama3_1(nn.Module):
     def __init__(self, args: ModelArgs):
@@ -28,10 +43,9 @@ class Llama3_1(nn.Module):
         self.norm = nn.BatchNorm1d(args.dim)
         self.lm_head = nn.Linear(in_features=args.dim, out_features=args.vocab_size, bias=False)
 
-    def forward(self, x: torch.Tensor, start_pos: int) -> torch.Tensor:
-        B, L = x.shape
-
-        embedds = self.embeddings(x)
+    def forward(self, tokens: torch.Tensor, start_pos: int) -> torch.Tensor:
+        B, L = tokens.shape
+        embedds = self.embeddings(tokens)
 
         freqs_cis = torch.Tensor
 
@@ -39,4 +53,8 @@ class Llama3_1(nn.Module):
         if L >= 1:
             print("Implementieren")
 
-        for layer in enumerate()
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_cis, mask)
+
+        h = self.norm(h)
+        return self.lm_head(h).float()
