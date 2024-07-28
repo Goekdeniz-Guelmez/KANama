8012f03d99152a4d6997d9a824b10a0a14fe80f4
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-28 22:54:02 +0200

    adding scaling

--- llama3_1.py
+++ llama3_1.py
@@ -17,6 +17,9 @@ class ModelArgs:
     num_kv_heads: Optional[int] = None
     rms_norm_eps: float = 1e-5
 
+    rope_theta: float = 500000
+    use_scaled_rope: bool = False
+
 
 
 class RMSNorm(torch.nn.Module):
@@ -31,20 +34,57 @@ class RMSNorm(torch.nn.Module):
     def forward(self, x):
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
-    
 
-class Llama3_1(nn.Module):
+
+def apply_scaling(freqs: torch.Tensor):
+    scale_factor = 8
+    low_freq_factor = 1
+    high_freq_factor = 4
+    old_context_len = 8192
+
+    low_freq_wavelen = old_context_len / low_freq_factor
+    high_freq_wavelen = old_context_len / high_freq_factor
+    new_freqs = []
+    for freq in freqs:
+        wavelen = 2 * math.pi / freq
+        if wavelen < high_freq_wavelen:
+            new_freqs.append(freq)
+        elif wavelen > low_freq_wavelen:
+            new_freqs.append(freq / scale_factor)
+        else:
+            assert low_freq_wavelen != high_freq_wavelen
+            smooth = (old_context_len / wavelen - low_freq_factor) / (high_freq_factor - low_freq_factor)
+            new_freqs.append((1 - smooth) * freq / scale_factor + smooth * freq)
+    return torch.tensor(new_freqs, dtype=freqs.dtype, device=freqs.device)
+
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0, use_scaled: bool = False):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device, dtype=torch.float32)
+    if use_scaled:
+        freqs = apply_scaling(freqs)
+    freqs = torch.outer(t, freqs)
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
+
+
+class Llama3_1Transformer(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
         self.args = args
 
-        self.freqs_cis = precompute_freqs_cis() # TODO
+        self.freqs_cis = precompute_freqs_cis(
+            args.dim // args.n_heads,
+            args.max_seq_len * 2,
+            args.rope_theta,
+            args.use_scaled_rope
+        )
 
-        self.embeddings = nn.Embedding(args.vocab_size, args.dim, padding_idx=)
+        self.embeddings = nn.Embedding(args.vocab_size, args.dim, padding_idx=args.padding_idx)
 
         self.layers = torch.nn.ModuleList()
         for layer_id in range(args.n_layers):
-            self.layers.append(Block(layer_id, args)) # TODO
+            self.layers.append(TransformerBlock(layer_id, args)) # TODO
 
         self.norm = RMSNorm(args.dim, args.rms_norm_eps)
         self.lm_head = nn.Linear(in_features=args.dim, out_features=args.vocab_size, bias=False)
@@ -54,16 +94,17 @@ class Llama3_1(nn.Module):
         B, L = tokens.shape
         embedds = self.embeddings(tokens)
 
+        self.freqs_cis = self.freqs_cis.to(embedds.device)
         freqs_cis = # TODO
 
         mask = None
         if L > 1:
             mask = torch.full((L, L), float("-inf"), device=tokens.device)
             mask = torch.triu(mask, diagonal=1)
-            mask = torch.hstack([torch.zeros((L, start_pos), device=tokens.device), mask]).type_as(h)
+            mask = torch.hstack([torch.zeros((L, start_pos), device=tokens.device), mask]).type_as(embedds)
 
         for layer in self.layers:
-            h = layer(h, start_pos, freqs_cis, mask)
+            h = layer(embedds, start_pos, freqs_cis, mask)
 
         h = self.norm(h)
         return self.lm_head(h).float()
