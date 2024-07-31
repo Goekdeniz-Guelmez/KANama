ed255d328c0f3c6deb68b638dfd337feb5fd95da
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-07-31 23:46:00 +0200

    upd

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,14 @@
 from .version import __version__
 
 def __getattr__(name):
+    if name in ("KANLinear", "KAN"):
+        from .kan import KANLinear, KAN
+        return locals()[name]
+    if name in ("ModelArgs"):
+        from .args import ModelArgs
+        return locals()[name]
     if name in ("KANamav1", "KANamav2", "KANamav3", "KANamav4"):
         from model import KANamav1, KANamav2, KANamav3, KANamav4
         return locals()[name]
 
-__all__ = ["KANamav1", "KANamav2", "KANamav3", "KANamav4"]
+__all__ = ["KANLinear", "KAN", "KANamav1", "KANamav2", "KANamav3", "KANamav4", "ModelArgs"]
